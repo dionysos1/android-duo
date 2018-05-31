1c21f7de35330267d1560541c4d4ab37dee3d2ef
Joshuaneuro <joshuavdmeij@gmail.com>
2018-05-31 11:51:21 +0200

    INITAL COMMIT

---
+++ app/src/main/java/com/example/joshua/projectduo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.joshua.projectduo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static com.example.joshua.projectduo.R.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_main);
+    }
+}
